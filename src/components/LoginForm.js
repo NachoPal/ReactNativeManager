ab493fd3fd6892e63a9816e342068b747d91832c
Nacho <ignacio.palacios.santos@gmail.com>
2017-01-13 17:05:04 +0100

    Login with spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { connect } from 'react-redux'
 import { emailChanged, passwordChanged, loginUser } from '../actions'
 
@@ -18,6 +18,17 @@ class LoginForm extends Component {
 
     this.props.loginUser( {email, password} )
   }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large"/>;
+    }
+    return(
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
+  };
 
   render(){
     return(
@@ -46,9 +57,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -67,7 +76,8 @@ const mapsStateToProps = state => {
   return {
     email: state.auth.email,
     password: state.auth.password,
-    error: state.auth.error
+    error: state.auth.error,
+    loading: state.auth.loading
   };
 };
 
